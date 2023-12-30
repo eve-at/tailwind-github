873e389ade5e86bdf1c3511ea65a61f143cd2502
Alexandre Muraviov <amuraviov@globalia.ca>
2023-12-30 16:42:58 -0500

    cut header into components

---
+++ src/components/MainMenuElement.vue
@@ -0,0 +1,29 @@
+<script setup>
+defineProps({
+    active: {
+        type: Boolean,
+        default: false
+    },
+    title: {
+        type: String
+    },
+    count: {
+        type: Number,
+        default: 0
+    }
+})
+</script>
+
+<template>
+    <a href="#" 
+        class="flex items-center px-4 pb-3 border-b-2"
+        :class="{ 
+            'border-red-500 font-semibold': active,
+            'border-transparent hover:border-gray-400 transition ease-in-out duration-150': !active
+        }"
+    >
+        <slot/>
+        <span class="ml-2 whitespace-nowrap">{{ title }}</span>
+        <div v-if="count > 0" class="bg-gray-200 rounded-lg px-2 text-xs ml-1">{{ count }}</div>
+    </a>
+</template>
---
+++ src/components/RoundedGroup.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Chevron from './Chevron.vue';
+
+defineProps({
+    title: {
+        type: String
+    },
+    count: {
+        type: String
+    }
+})
+</script>
+
+<template>
+    <div class="flex text-xs">
+        <button class="border border-gray-400 rounded rounded-r-none px-3 py-1 flex items-center space-x-1">
+        <slot />
+        <div>{{ title }}</div>
+        <Chevron />
+        </button>
+        <a href="#" class="border border-gray-400 rounded rounded-l-none border-l-0 font-semibold px-3 py-1 hover:text-blue-600">
+            {{ count }}
+        </a>
+    </div>
+</template>
